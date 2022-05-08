55ada53f6d09c458911b50dd7a477fc92d5c7d28
D4sherInc <phil.klotzsche@gmail.com>
2022-05-08 13:07:19 +0200

    ASum: find n for given m so that: m = Σ(i^3) from i=1 to n (WIP)

---
+++ src/java/main/ASum.java
@@ -0,0 +1,18 @@
+public class ASum {
+    public static long findNb(long m) {
+        int n = 0;
+        for (long i = 0L; i <= m; n++) {
+            if (i == m) return n;
+            i += Math.pow(n + 1, 3);
+        }
+        return -1;
+    }
+
+    public static long testFailed(long m) {
+        int n = 0;
+        for (long i = 0L; i <= m; i++) {
+            n += Math.pow(i, 3);
+        }
+        return n;
+    }
+}
