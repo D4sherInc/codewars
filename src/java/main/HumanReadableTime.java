4475cbf4dbd7882ff611f8f470ca7bc1b3141952
D4sherInc <phil.klotzsche@gmail.com>
2022-05-11 17:50:21 +0200

    HumanReadableTime: shorter version

--- src/java/main/HumanReadableTime.java
+++ src/java/main/HumanReadableTime.java
@@ -4,37 +4,11 @@ public class HumanReadableTime {
 
     //HH:MM:SS
     public static String makeReadable(int seconds) {
-        if (seconds / 359999 > 1) return "99:59:59";
-        StringBuilder result = new StringBuilder("");
+        int hours = seconds / 3600;
+        int minutes = seconds % 3600 / 60;
+        int secs = seconds % 60;
 
-        //hours
-        if (seconds / 3600 > 0) {
-            int hours = seconds / 3600;
-            // starting 0 if less then 10
-            // or else it would look like: 1:00:00
-            if (hours / 10 < 1) result.append("0");
-            result.append(hours).append(":");
-            seconds = seconds % 3600;
-        }
-        else result.append("00:");
+        return String.format("%02d:%02d:%02d", hours, minutes, secs);
 
-        //minutes
-        if (seconds / 60 > 0) {
-            int minutes = seconds / 60;
-            // starting 0 if less then 10
-            // or else it would look like: 00:1:00
-            if (minutes / 10 < 1) result.append("0");
-            result.append(minutes).append(":");
-            seconds = seconds % 60;
-        }
-        else result.append("00:");
-
-        //seconds
-        // starting 0 if less then 10
-        // or else it would look like: 00:00:1
-        if (seconds / 10 < 1) result.append("0");
-        result.append(seconds);
-
-        return result.toString();
     }
 }
