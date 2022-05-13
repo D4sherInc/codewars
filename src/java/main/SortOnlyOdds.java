d6bd244f1659d21bc30e4dfe23e48f687f802121
D4sherInc <phil.klotzsche@gmail.com>
2022-05-13 17:27:30 +0200

    SortOnlyOdds: init

---
+++ src/java/main/SortOnlyOdds.java
@@ -0,0 +1,7 @@
+//task:
+//https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/java
+public class SortOnlyOdds {
+    public static int[] sortArray(int[] array) {
+        return array;
+    }
+}
