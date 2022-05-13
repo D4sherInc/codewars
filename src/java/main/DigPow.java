27a1ba331240313424884bcda15b51e7f14747d6
D4sherInc <phil.klotzsche@gmail.com>
2022-05-13 16:30:14 +0200

    DigPow: init

---
+++ src/java/main/DigPow.java
@@ -0,0 +1,10 @@
+//task:
+//https://www.codewars.com/kata/5552101f47fc5178b1000050/train/java
+public class DigPow {
+
+    public static long digPow(int n, int p) {
+        // your code
+        return -1;
+    }
+
+}
