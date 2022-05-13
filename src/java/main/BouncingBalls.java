eb66110e777c9bd7a7e2309017c9e55480fee844
D4sherInc <phil.klotzsche@gmail.com>
2022-05-13 16:28:24 +0200

    BouncingBalls: finished

--- src/java/main/BouncingBalls.java
+++ src/java/main/BouncingBalls.java
@@ -1,6 +1,14 @@
 //task:
 // https://www.codewars.com/kata/5544c7a5cb454edb3c000047/java
 public class BouncingBalls {
+    public static int bouncingBall(double height, double bounce, double window) {
+        if (height <= 0.0 || bounce <= 0.0 || bounce >= 1.0 || window >= height) return -1;
+        int bounces = 0;
 
-
+        for (int n = 0; height > window; bounces++) {
+             if (n%2 == 0) height = height* bounce;
+             n++;
+        }
+        return bounces;
+    }
 }
--- src/java/test/BouncingBallsTest.java
+++ src/java/test/BouncingBallsTest.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class BouncingBallsTest {
+public class BouncingBallsTest {
 
+    @Test
+    public void test1() {
+        assertEquals(3, BouncingBalls.bouncingBall(3.0, 0.66, 1.5));
+    }
+    @Test
+    public void test2() {
+        assertEquals(15, BouncingBalls.bouncingBall(30.0, 0.66, 1.5));
+    }
 }
