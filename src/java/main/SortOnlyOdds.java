10a003c28a0be3871206022621896cf60cb2cb73
D4sherInc <phil.klotzsche@gmail.com>
2022-05-13 17:47:25 +0200

    SortOnlyOdds: done

--- src/java/main/SortOnlyOdds.java
+++ src/java/main/SortOnlyOdds.java
@@ -1,7 +1,30 @@
+import java.util.LinkedList;
+import java.util.List;
+
 //task:
 //https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/java
 public class SortOnlyOdds {
     public static int[] sortArray(int[] array) {
-        return array;
+        boolean[] isEven = new boolean[array.length];
+        List<Integer> odds = new LinkedList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) isEven[i] = true;
+            else {
+                isEven[i] = false;
+                odds.add(array[i]);
+            }
+        }
+        odds.sort(Integer::compare);
+
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++){
+            if (isEven[i]) result[i] = array[i];
+            else {
+                result[i] = odds.get(0);
+                odds.remove(0);
+            }
+        }
+
+        return result;
     }
 }
