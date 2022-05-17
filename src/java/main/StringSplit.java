aa20d51664a2b61de986b465b2361d694d02f0a0
D4sherInc <phil.klotzsche@gmail.com>
2022-05-17 16:30:54 +0200

    StringSplit: init

---
+++ src/java/main/StringSplit.java
@@ -0,0 +1,8 @@
+// task:
+// https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/java
+
+public class StringSplit {
+    public static String[] solution(String s) {
+        return new String[];
+    }
+}
