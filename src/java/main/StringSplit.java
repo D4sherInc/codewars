4711508ad69f891aaeb875f4081e4a132a8d68f7
D4sherInc <phil.klotzsche@gmail.com>
2022-05-17 16:53:24 +0200

    StringSplit: done

--- src/java/main/StringSplit.java
+++ src/java/main/StringSplit.java
@@ -3,6 +3,23 @@
 
 public class StringSplit {
     public static String[] solution(String s) {
-        return new String[];
+
+        String[] result = new String[ ((s.length()+1)/2)];
+
+        int i = 0;
+
+        while(!s.equals("")) {
+            if (s.length() > 1) {
+                result[i] = s.substring(0,2);
+                s = s.substring(2);
+            }
+            else {
+                result[i] = s.substring(0,1).concat("_");
+                s = s.substring(1);
+            }
+            i++;
+        }
+
+        return result;
     }
 }
