935de397bfce111fc4fa2dd156ad9bfd32219ea7
D4sherInc <phil.klotzsche@gmail.com>
2022-05-22 18:12:35 +0200

    divisors.pl: done

--- src/prolog/divisors.pl
+++ src/prolog/divisors.pl
@@ -1,1 +1,14 @@
-divisors(Number, Result) :- Result is 0.
+divisors(Number, Count) :- 
+	Number >= 1,
+	divisors(Number, 1, 1, Count).
+
+
+divisors(Number, Number, Count, Count):- !.
+divisors(Number, N, Count, R) :-
+	(0 is Number mod N ->
+		succ(Count, NewCount)
+	;
+		NewCount is Count
+	),
+	succ(N, NewN),
+	divisors(Number, NewN, NewCount, R).
